3425fb24d519412e55b259ef0039e24e32c4d950
Narayan Aryal <aryal.coding@gmail.com>
2022-10-18 12:26:22 +1100

    counter project done

--- script.js
+++ script.js
@@ -7,6 +7,27 @@ const btns = document.querySelectorAll(".btn");
 btns.forEach((btn, i) => {
     btn.addEventListener("click", ((a) =>{
         const styles = a.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }else if(styles.contains("increase")){
+            count++;
+        }else{
+            count = 0;
+        };
+
+        if(count > 0){
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "red"
+        }
+
+        if(count === 0){
+            value.style.color = "black"
+        }
+
+
+        value.textContent = count;
       
-    }))
-})
+    }));
+});
